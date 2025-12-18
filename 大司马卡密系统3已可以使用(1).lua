375b3a8ccba566aa50ee74bfe18521dea611e910
whenheet <3579175593@qq.com>
2025-12-18 18:35:38 +0800

    Update 大司马卡密系统3已可以使用(1).lua

--- 大司马卡密系统3已可以使用(1).lua
+++ 大司马卡密系统3已可以使用(1).lua
@@ -115,6 +115,7 @@ Button.MouseButton1Click:Connect(function()
        ["shenchou9178"] = {user = "shenchou888", expire = "2030-12-31"},
        ["eud4889dfjkl"] = {user = "LMK2854562558", expire = "2030-12-31"},
        ["eud4gs866"] = {user = "rroorroororrooror", expire = "2030-12-31"},
+       ["ghk4gs664"] = {user = "FJHC1222", expire = "2030-12-31"},
        ["h5Z6D4D2"] = {user = "12345678xto", expire = "2030-12-31"}
         -- 可复制上面的格式，添加更多卡密
     }
