76f7ab7c2dfa63cff016c6890d18d4704411ec63
whenheet <3579175593@qq.com>
2025-12-25 11:59:47 +0800

    Add new user entry to 大司马卡密系统

--- 大司马卡密系统3已可以使用(1).lua
+++ 大司马卡密系统3已可以使用(1).lua
@@ -147,6 +147,7 @@ Button.MouseButton1Click:Connect(function()
        ["skefwmx41l"] = {user = "GTHHGSH", expire = "2030-12-31"},
        ["ihdjk45kl"] = {user = "cheems070828", expire = "2030-12-24"},
        ["王皓9178"] = {user = "yutidhdivd", expire = "2030-12-24"},
+       ["wygsnvko"] = {user = "zdclll2", expire = "2030-12-24"},
        ["h5Z6D4D2"] = {user = "12345678xto", expire = "2030-12-31"}
         -- 可复制上面的格式，添加更多卡密
     }
